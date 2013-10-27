e8d633c214926ef3f99511e282da40b68331b33b
Paul Brook <paul@nowt.org>
2013-10-27 08:30:10 +0000

    Fix state loading
    Too much $this
    
    Signed-off-by: Paul Brook <paul@nowt.org>

--- src/werewolfsms/Werewolfsms/GameController.php
+++ src/werewolfsms/Werewolfsms/GameController.php
@@ -302,7 +302,7 @@ class GameController
     {
         $ar = json_decode($json, true);
         $this->people = $this->storage->getAllPeople();
-        $this->phase = $this->$ar["phase"];
+        $this->phase = $ar["phase"];
         $this->moninator = $this->toPerson($ar["nominator"]);
         $this->seconder = $this->toPerson($ar["seconder"]);
         $this->accused = $this->toPerson($ar["accused"]);
